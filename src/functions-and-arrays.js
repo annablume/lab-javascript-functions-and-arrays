c7cf3a680f0c32994d7f0400bbc1621f520a6d24
Antje Oltmanns <antje.oltmanns@gmail.com>
2021-01-12 21:38:48 +0100

    inProgress

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,15 +1,38 @@
 // Iteration #1: Find the maximum
 
+function maxOfTwoNumbers(a, b) {
+  if (a > b) {
+    return a;
+ }  return b;
+ } 
+
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+function findLongestWord(words) {
+  for (let i = 0; i < words.length; i++) {
+    if (words.lenght === 0) {
+      return null;
+    } 
+  }
+}
+
+
 
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+function sumNumbers() {
+  
+}
+
+
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+
+
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
